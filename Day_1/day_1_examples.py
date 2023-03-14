498e3c257c942464a2cdb9c14763669c627736ed
French-Austin1 <austin.french@smoothstack.com>
2023-03-13 19:14:27 -0700

    Added dict example

--- Day_1/day_1_examples.py
+++ Day_1/day_1_examples.py
@@ -3,36 +3,63 @@
 my_tuple = (10, 20, 30)
 my_str = ("Hello World!", "hello")
 
+# Adding tuples together to make a new tuple
 a = my_tuple + my_str
 
-# a = list(my_tuple)
-# a.append(my_str)
-# my_tuple = tuple(a)
+# Casting a tuple to a list in order to append an item.
+a = list(my_tuple)
+a.append(my_str)
+my_tuple = tuple(a)
 
-
-# print(my_tuple)
 
 '''Lists'''
 
-# my_list = ["Jason","Austin","Taite"]
+my_list = ["Jason","Austin","Taite"]
 
-# print(my_list[:-1])
+# Return a the list in reverse order
+my_list[:-1] 
+
+# Get an element out of a list (Zero indexed)
+my_list[2]
+
+# Add an element to the end of the list
+my_list.append("Bryan")
 
 '''Sets'''
 
-my_set = {"Austin", "Bryan", "Nathen", "Austin"}
+my_set = {"Austin", "Bryan", "Nathen"}
+your_set = {"Kyle", "Bryan", "Tyler"}
 
-# my_set.add()
-# my_set.update()
-# my_set.difference()
-# my_set.discard()
+# add an element to a set. This would not make any change to the set as it already exists within it.
+my_set.add("Austin")
 
+# Find the difference between sets
+diff = my_set.difference(your_set)
 
+# Update a set with a union of itself and others
+my_set.update()
+
+# Remove an element from the set
+my_set.discard("Austin")
+
+# What would my_set be here?
 print(my_set)
 
 '''dicts'''
 
 my_dict = {"First Name":"Austin", }
+
+# you can add items to a dict by using a new Key as an index
 my_dict["Last Name"] = "French"
 
-print(my_dict.get("First_Name"))
+# use the .get() function to get specific values, if you use this 
+my_dict.get("First Name")
+
+# You can also get the value by calling an index in the dict
+my_dict["First Name"]
+
+# Returns a set-like object of all the of the keys, the same can be done with .values()
+my_dict.keys()
+my_dict.values()
+
+
