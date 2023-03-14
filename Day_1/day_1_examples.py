4ab977fe34b2aa2585d83858e38a012b206bec66
French-Austin1 <austin.french@smoothstack.com>
2023-03-13 17:11:45 -0700

    Added day one examples

---
+++ Day_1/day_1_examples.py
@@ -0,0 +1,38 @@
+'''tuples'''
+
+my_tuple = (10, 20, 30)
+my_str = ("Hello World!", "hello")
+
+a = my_tuple + my_str
+
+# a = list(my_tuple)
+# a.append(my_str)
+# my_tuple = tuple(a)
+
+
+# print(my_tuple)
+
+'''Lists'''
+
+# my_list = ["Jason","Austin","Taite"]
+
+# print(my_list[:-1])
+
+'''Sets'''
+
+my_set = {"Austin", "Bryan", "Nathen", "Austin"}
+
+# my_set.add()
+# my_set.update()
+# my_set.difference()
+# my_set.discard()
+
+
+print(my_set)
+
+'''dicts'''
+
+my_dict = {"First Name":"Austin", }
+my_dict["Last Name"] = "French"
+
+print(my_dict.get("First_Name"))
