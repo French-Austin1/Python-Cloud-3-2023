ac43dfda8e910e6d2b8c19bc87fb920f3207790c
French-Austin1 <austin.french@smoothstack.com>
2023-03-17 06:23:24 -0700

    Added some changes to the examples

--- Day_4/examples/rotating_file_handler.py
+++ Day_4/examples/rotating_file_handler.py
@@ -11,3 +11,11 @@ logger.setLevel(logging.DEBUG)
 
 logger.debug('This is a debug message')
 logger.info('This is an info message')
+
+
+DEBUG = 0
+INFO = 1
+FATAL = 2
+ERROR = 3
+CRITICAL = 4 
+
--- Day_4/exercises/Flask.py
+++ Day_4/exercises/Flask.py
@@ -1,15 +1,29 @@
+from flask import Flask, render_template
 
 
 def exercise_1():
     '''Create a Flask application that returns "Hello, World!"
     when the root URL is requested (/). Use the @app.route decorator to define the route.'''
-    pass
+    app = Flask(__name__)
+
+    @app.route('/')
+    def method_name():
+        return "<h1>Hello, World!</h1>"
+    
+    return app
+    
 
 def exercise_2():
     '''Create a Flask application that uses an HTML template to render a simple webpage.
     The webpage should include a header, a paragraph of text, and an image.
     You can use the render_template function to render the HTML template.'''
-    pass
+    app = Flask(__name__)
+    @app.route('/')
+    @app.route('/home')
+    def method_name():
+        return render_template("home.html")
+    
+    return app
 
 def exercise_3():
     '''Create a Flask application that includes API endpoints for retrieving and updating data. The application should 
@@ -17,3 +31,8 @@ def exercise_3():
     You can use a simple data structure like a dictionary to store the data.
     '''
     pass
+
+
+if __name__ == "__main__":
+    app = exercise_1()
+    app.run()
